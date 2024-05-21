da7bc2d14cd9b3730f22ebf57176843d166cea6e
ANAJJJJJ <150226355+ANAJJJJJ@users.noreply.github.com>
2024-05-21 14:52:25 +0530

    Add files via upload

---
+++ JSDAY1/script.js
@@ -0,0 +1,64 @@
+
+// let person={
+    // fname : "Anaj Ajayan",
+    // address :{
+        // street : "Irinjalakuda",
+        // place : "Thrisur",
+        // pin : "680688"
+        // },
+    // age : 10
+// }
+// console.log(person.address.place)
+// console.log(person.age)
+
+
+// var toothbrush = new Object();
+// toothbrush.brand = "Colgate";
+// toothbrush.colour = "blue";
+// console.log(toothbrush);
+
+
+// concatination
+// let fname = "Anaj";
+// let lname ="Ajayan";
+// console.log(`${fname} and ${lname}`)
+
+
+// let m =5;
+// let n =6;
+// console.log(`the sum is ${m+n}`)
+
+
+// Array
+// let fruits = ["Apple","Orange","Mnago",23]
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits.length);
+// fruits.push("Grapes");
+// console.log(fruits);
+// fruits[2]="pineapple;"
+// console.log(fruits);
+// fruits.unshift("pear");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+
+// Array of object
+// let bsc =[
+    // {
+    // fname: "Anaj",
+    // age: "18",
+    // },
+    // {
+    // fname: "Ashwin",
+    // age: "19",
+    // },
+    // {
+    // fname: "Donal",
+    // age: "20",
+    // },
+// ];
+// console.log(bsc[2].age);
